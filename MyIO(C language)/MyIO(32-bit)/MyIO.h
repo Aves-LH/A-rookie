b55b51488b4740968b9e4d5743e1faf8c4eb14cd
Aves-LH <53402172+Aves-LH@users.noreply.github.com>
2019-12-09 11:38:01 +0800

    Create MyIO.h

---
+++ MyIO(C language)/MyIO(32-bit)/MyIO.h
@@ -0,0 +1,11 @@
+#ifndef __MYIO_H__
+#define __MYIO_H__
+
+char* GetString(char *pBuf);
+int AtoI(int nSum, char *pStr);
+void Scanf(const char *pFormat, ...);
+
+void ItoA(int nNum, char *pStr);
+void Print(const char *pFormat, ...);
+
+#endif 
