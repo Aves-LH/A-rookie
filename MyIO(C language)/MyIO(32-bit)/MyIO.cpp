1868cec3604793cee083fe49748d2d9fe56fe187
Aves-LH <53402172+Aves-LH@users.noreply.github.com>
2019-12-09 11:56:15 +0800

    Update MyIO.cpp

--- MyIO(C language)/MyIO(32-bit)/MyIO.cpp
+++ MyIO(C language)/MyIO(32-bit)/MyIO.cpp
@@ -1,127 +1,6 @@
 #include "pch.h"
 #include "MyIO.h"
 
-char* GetString(char *pBuf)
-{
-	if (NULL != pBuf)
-	{
-		//*pBuf = getchar();
-		char *pTemp = pBuf;
-
-
-		while (true)
-		{
-			*pTemp = getchar();
-			if (' ' == *pTemp || '\n' == *pTemp)
-			{
-				break;
-			}
-			pTemp++;
-		}
-		*pTemp = '\0';
-	}
-
-	return pBuf;
-}
-
-int AtoI(int nSum, char *pStr)
-{
-	if (NULL != pStr)
-	{
-		int nLen = strlen(pStr);
-		int nNum2 = nLen;
-		int nNum = 0;
-
-		if ('-' == pStr[0])
-		{
-			for (int i = 1; i < nNum2 && nLen > 0; i++, nLen--)
-			{
-				int nNum1 = 1;
-				for (int j = 1; j < nLen - 1; j++)
-				{
-					nNum1 *= 10;
-				}
-				nNum = (pStr[i] - '0') * nNum1;
-				nSum += nNum;
-			}
-		}
-		else
-		{
-			for (int i = 0; i < nNum2 && nLen > 0; i++, nLen--)
-			{
-				int nNum1 = 1;
-				for (int j = 1; j < nLen; j++)
-				{
-					nNum1 *= 10;
-				}
-				nNum = (pStr[i] - '0') * nNum1;
-				nSum += nNum;
-			}
-		}
-	}
-
-	if ('-' == pStr[0])
-	{
-		return -nSum;
-	}
-	else
-	{
-		return nSum;
-	}
-}
-
-
-
-void Scanf(const char *pFormat, ...)
-{
-	if (NULL != pFormat)
-	{
-		char **pTemp = (char **)&pFormat;
-		pTemp++;
-
-
-		while ('\0' != *pFormat)
-		{
-			if ('%' == *pFormat)
-			{
-				pFormat++;
-				switch (*pFormat)
-				{
-				case 'd':
-				{
-					char szBuf[20] = "";
-					char *pBuf = NULL;
-
-					int nNum = 0;
-					pBuf = GetString(szBuf);
-
-					**(int **)pTemp = AtoI(nNum, pBuf);
-
-					pTemp++;
-				}
-				break;
-				case 'c':
-				{
-					**(char **)pTemp = getchar();
-
-					pTemp++;
-				}
-				break;
-				case 's':
-				{
-					GetString(*pTemp);
-
-					pTemp++;
-				}
-				break;
-				}
-			}
-
-			pFormat++;
-		}
-	}
-}
-
 void ItoA(int nNum, char *pStr)
 {
 	if (NULL != pStr)
@@ -147,8 +26,8 @@ void Print(const char *pFormat, ...)
 {
 	if (NULL != pFormat)
 	{
-		const char **pTemp = &pFormat;
-		pTemp++;
+		char *pTemp = (char *)&pFormat;
+		pTemp += 4;
 
 		while ('\0' != *pFormat)
 		{
@@ -159,39 +38,39 @@ void Print(const char *pFormat, ...)
 				{
 				case 'c':
 				{
-					putchar(**pTemp);
-					pTemp++;
+					putchar(*pTemp);
+					pTemp += 4;
 				}
 				break;
 				case 'd':
 				{
-					char szBuf[20] = "";
-					int nNum = *(int *)pTemp;
+					char szBuf[255] = "";
+					int nNum = 0;
 
-					ItoA(nNum, szBuf);
+					ItoA(*(int *)pTemp, szBuf);
 
 					for (int i = 0; '\0' != szBuf[i]; i++)
 					{
 						putchar(szBuf[i]);
 					}
 
-					pTemp++;
+					pTemp += 4;
 				}
 				break;
 				case 's':
 				{
-					for (int i = 0; '\0' != (*pTemp)[i]; i++)
+					for (int i = 0; '\0' != (*(char **)pTemp)[i]; i++)
 					{
-						putchar((*pTemp)[i]);
+						putchar((*(char **)pTemp)[i]);
 					}
 
-					pTemp++;
+					pTemp += 4;
 				}
 				break;
 				default:
 				{
 					pFormat--;
-					putchar(*pFormat);
+					putchar(*pTemp);
 				}
 				break;
 				}
@@ -200,7 +79,112 @@ void Print(const char *pFormat, ...)
 			{
 				putchar(*pFormat);
 			}
+			pFormat++;
+		}
+	}
+}
 
+char* GetBuf(char *pBuf)
+{
+	if (NULL != pBuf)
+	{
+		//*pBuf = getchar();
+		char *pTemp = pBuf;
+
+		while (true)
+		{
+			*pTemp = getchar();
+			if (' ' == *pTemp || '\n' == *pTemp)
+			{
+				break;
+			}
+			pTemp++;
+		}
+		*pTemp = '\0';
+	}
+	return pBuf;
+}
+
+int AtoI(int nRes, const char *pStr)
+{
+	if (NULL != pStr)
+	{
+		int nNum = 1;
+		int nLen = StrLen(pStr);
+
+		if ('-' == pStr[0])
+		{
+			for (int i = 1; '\0' != pStr[i]; i++)
+			{
+				nNum = pStr[i] - '0';
+				for (int j = nLen - 2; j > 0; j--)
+				{
+					nNum *= 10;
+				}
+				nLen--;
+				nRes += nNum;
+			}
+			return -nRes;
+		}
+		else
+		{
+			for (int i = 0; '\0' != pStr[i]; i++)
+			{
+				nNum = pStr[i] - '0';
+				for (int j = nLen - 1; j > 0; j--)
+				{
+					nNum *= 10;
+				}
+				nLen--;
+				nRes += nNum;
+			}
+			return nRes;
+		}
+	}
+	return 0;
+}
+
+void Scanf(const char *pFormat, ...)
+{
+	if (NULL != pFormat)
+	{
+		char *pTemp = (char *)&pFormat;
+		pTemp += 4;
+
+		while ('\0' != *pFormat)
+		{
+			if ('%' == *pFormat)
+			{
+				pFormat++;
+				switch (*pFormat)
+				{
+				case 'c':
+				{
+					**(char **)pTemp = getchar();
+					pTemp += 4;
+					getchar();
+				}
+				break;
+				case 'd':
+				{
+					char szBuf[255] = "";
+					int nNum = 0;
+					char *pBuf = GetBuf(szBuf);
+
+					**(int **)pTemp = AtoI(nNum, pBuf);
+
+					pTemp += 4;
+				}
+				break;
+				case 's':
+				{
+					GetBuf(*(char **)pTemp);
+
+					pTemp += 4;
+				}
+				break;
+				}
+			}
 
 			pFormat++;
 		}
